84133c5672c22a95fe418b80a137a6c6ec5b8cdd
yeferson <yefersonapk@gmail.com>
2024-03-02 17:13:47 -0500

    one

---
+++ apurimac/script.js
@@ -0,0 +1,70 @@
+// Obtener una referencia al elemento canvas del DOM
+const $grafica = document.querySelector("#grafica");
+// Las etiquetas son las que van en el eje X. 
+const etiquetas = ["Enero", "Febrero", "Marzo", "Abril"]
+// Podemos tener varios conjuntos de datos. Comencemos con uno
+const datosVentas2020 = {
+    label: "CANTIDAD DE SOLICITUDES POR MES (BUROCRACIA)",
+    data: [1000, 1500, 8000, 3000], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+    borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
+    borderWidth: 1,// Ancho del borde
+};
+new Chart($grafica, {
+    type: 'bar',// Tipo de gráfica
+    data: {
+        labels: etiquetas,
+        datasets: [
+            datosVentas2020,
+            // Aquí más datos...
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+        },
+    }
+});
+
+
+
+
+
+
+
+// Obtener una referencia al elemento canvas del DOM
+const $grafica2 = document.querySelector("#grafica2");
+// Las etiquetas son las que van en el eje X. 
+const etiquetas2 = ["Enero", "Febrero", "Marzo", "Abril"]
+// Podemos tener varios conjuntos de datos. Comencemos con uno
+const datosVentas20202 = {
+
+    label: "CANTIDAD DE SOLICITUDES NO RESPONDIDAS POR MES (BUROCRACIA)",
+    data: [5000, 1500, 8000, 5102], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(252, 11, 0, 0.4)', // Color de fondo
+    borderColor: 'rgba(252, 11, 0, 1)', // Color del borde
+    borderWidth: 1,// Ancho del borde
+};
+new Chart($grafica2, {
+    type: 'bar',// Tipo de gráfica
+    data: {
+        labels: etiquetas2,
+        datasets: [
+            datosVentas20202,
+            // Aquí más datos...
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+        },
+    }
+});
